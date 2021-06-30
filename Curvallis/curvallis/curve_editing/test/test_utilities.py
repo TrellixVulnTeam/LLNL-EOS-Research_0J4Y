09804e452ee4f83560fd1ae2c14d3f0b464d2e54
Wentinn Liao <wentinn.liao@gmail.com>
2021-06-29 21:45:13 -0700

    TF density normalization update

--- Curvallis/curvallis/curve_editing/test/test_utilities.py
+++
@@ -1,6 +0,0 @@
-from collections import namedtuple
-
-
-def make_args(**kwargs):
-    Args = namedtuple('Args', kwargs.keys())
-    return Args(*kwargs.values())
--- Curvallis/curvallis/version.py
+++
@@ -1,3 +0,0 @@
-# This file is auto-generated. Any changes made to this file will be overwritten.
-version = 'Mon 10 May 2021 10:25:59 PM UTC'
-
